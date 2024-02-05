292bbad5b40b4754c08be276e32a182200218740
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2024-02-05 16:04:28 -0300

    showing the game status on the screen

--- src/App.js
+++ src/App.js
@@ -23,9 +23,16 @@ export default function Board() {
     setSquares(nextSquares)
     setXIsNext(!xIsNext)
   }
-
+  const winner = CalculateWinner(squares)
+  let status
+  if (winner) {
+    status = "Winner: " + winner
+  } else {
+    status = "Next player is: " + (xIsNext ? "X" : "O")
+  }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
