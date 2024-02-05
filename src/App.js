ed6c1290e29f98d58c1a3c6d3b47d69e87b3ddb9
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2024-02-05 16:27:09 -0300

    create a game history and raising states

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function Square({ value, onSquareClick }) {
     </button>
   )
 }
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || CalculateWinner(squares)) {
       return
@@ -20,8 +18,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O"
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
   }
   const winner = CalculateWinner(squares)
   let status
@@ -51,7 +48,26 @@ export default function Board() {
     </>
   )
 }
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length - 1]
 
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares])
+    setXIsNext(!xIsNext)
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{}</ol>
+      </div>
+    </div>
+  )
+}
 function CalculateWinner(squares) {
   const lines = [
     [0, 1, 2],
