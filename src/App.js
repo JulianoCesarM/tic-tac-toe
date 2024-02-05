c28f24b096fa1a72b5a7dfe7e9d53332508c5d18
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2024-02-05 16:02:47 -0300

    adding the match win condition calculation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || CalculateWinner(squares)) {
       return
     }
     const nextSquares = squares.slice()
@@ -23,6 +23,7 @@ export default function Board() {
     setSquares(nextSquares)
     setXIsNext(!xIsNext)
   }
+
   return (
     <>
       <div className="board-row">
@@ -42,4 +43,24 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+function CalculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let index = 0; index < lines.length; index++) {
+    const [a, b, c] = lines[index]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
